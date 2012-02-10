588089c0d60ec3ab92fd4614c7c980ee6b9da2ee
BBakerpkt <bbakerpkt@gmail.com>
2012-02-10 11:17:32 -0800

    Trying to make it work

--- config.ru
+++ config.ru
@@ -1,6 +1,5 @@
-require 'heroku-sinatra-app'
+require './heroku-sinatra-app'
 
-## There is no need to set directories here anymore;
-## Just run the application
+
 
 run Sinatra::Application
--- heroku-sinatra-app.rb
+++ heroku-sinatra-app.rb
@@ -8,14 +8,6 @@
 require 'rubygems'
 require 'sinatra'
 
-configure :production do
-  # Configure stuff here you'll want to
-  # only be run at Heroku at boot
-
-  # TIP:  You can get you database information
-  #       from ENV['DATABASE_URI'] (see /env route below)
-end
-
 # Quick test
 get '/' do
   "Congradulations!
